3899ede99b21f55a8909eb50129ac613fa432a57
Danny Blackstock <dannyblackstock@gmail.com>
2014-03-28 16:12:27 -0700

    Scale the image so it's easier to see.

--- main.lua
+++ main.lua
@@ -15,16 +15,18 @@ collisionMatrix = {}
 alert = ""
 
 function love.load()
-	--love.window.setMode(150, 150)
+	love.window.setMode(500, 600)
 
 	imageData = love.image.newImageData("colors.png")
 	image = love.graphics.newImage(imageData)
+	-- to fix blurry image scaling
+	image:setFilter( 'nearest', 'nearest' )
 
 	for i = 1, cellCount do
 		local img = love.image.newImageData("space.png")
 		if i > 10 and i <= 20 then
 			img = love.image.newImageData("space2.png")
-		end 
+		end
 		if i > 20 then
 			img = love.image.newImageData("white.png")
 		end
@@ -36,13 +38,16 @@ function love.load()
 
 	for x = 1, 101 do
 		collisionMatrix[x] = {}
-		for y = 1, 101 do 
+		for y = 1, 101 do
 			collisionMatrix[x][y] = 0
 		end
 	end
 end
 
 function love.draw()
+	-- scale the image so it's easier to see
+	love.graphics.push()
+	love.graphics.scale(5)
 
 	for i = 1, table.getn(cellModel) do
 		local tempCell = cellModel[i]
@@ -54,10 +59,10 @@ function love.draw()
 		end
 	end
 
-
-	love.graphics.print(alert, 0, 200)
-	alert = ""
-
 	image:refresh()
 	love.graphics.draw(image, 0, 0)
+	love.graphics.pop()
+
+	love.graphics.print(alert, 0, 500)
+	alert = ""
 end
